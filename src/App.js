f2913828b6ba35a65d1f1841c32b16147cd11a9c
Kishore Kuppan <kkuppan@vmware.com>
2024-08-18 15:41:02 +0530

    Fix: Payment base

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 import RegistrationPage from './component/page/registration-page/registration-page';
 import PrivacyPolicyComponent from './component/page/privacy-policy/privacy-policy';
 import TermsAndConditionsComponent from './component/page/terms-and-condtion/terms-and-condtion';
+import PaymentSuccess from './component/page/payment-success/payment-success';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/register-for-session" element={<RegistrationPage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicyComponent />} />
         <Route path="/terms-and-conditions" element={<TermsAndConditionsComponent />} />
+        <Route path="/payment-success" element={<PaymentSuccess />} />
         <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
     </BrowserRouter>
--- src/model/registration-page/registration-form.js
+++ src/model/registration-page/registration-form.js
@@ -1,5 +1,9 @@
+import axios from "axios";
+import { useState } from "react";
+// import { useEffect } from "react";
 import { Field, reduxForm } from "redux-form";
 import InputComponent from "../../component/organism/input-component/input-component";
+import RenderRazorpay from "../../component/page/payment-page/payment-page";
 import validators from "../../utilities/validator";
 
 const formValidators = {
@@ -19,6 +23,85 @@ const RegistrationForm = props => {
   const onSubmit = (values, dispatch, props) => {
     console.log(values)
   }
+
+  const razorpayKey = {
+    id: 'rzp_live_tG7YenWAYDDai9',
+    secret: 'POPBlMLZPVUGxhFiS2RE2OYb'
+  }
+
+
+  var [displayRazorpay, setDisplayRazorpay] = useState(false);
+  // var [offerEnded, changeOfferStatus] = useState(false);
+  var [orderDetails, setOrderDetails] = useState({
+    orderId: null,
+    currency: null,
+    amount: null,
+  });
+
+  // fake key
+  // key id rzp_test_0KFXQsngC14hgS
+  // key secret cl3tpS0AhzT1Ly3EKaM787jQ
+  function getApiResponse() {
+    const payload = {
+      amount: 100, //convert amount into lowest unit. here, Dollar->Cents
+      currency: 'INR',
+      keyId: 'rzp_live_tG7YenWAYDDai9',
+      KeySecret: 'POPBlMLZPVUGxhFiS2RE2OYb',
+    }
+    // const payload = { amount: 10, currency: 'INR', receipt: 'receipt#1', notes: {} }
+    const axiosHttp = axios.create({
+      baseURL: 'http://localhost:5000'
+    });
+    axiosHttp.post('/order', payload).then(resp => {
+      const data = resp
+        if(data && data.data?.order_id){
+          setOrderDetails({
+            orderId: data.data.order_id,
+            currency: data.data.currency,
+            amount: data.data.amount,
+          });
+          setDisplayRazorpay(true);
+        }
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  // function getApiResponse() {
+  //   const payload = { amount: 10, currency: 'INR', receipt: 'receipt#1', notes: {} };
+  //   const axiosHttp = axios.create({
+  //     baseURL: 'http://localhost:5000'
+  //   });
+  //   axiosHttp.post('/create-order', payload).then(resp => {
+  //     console.log(resp);
+
+  //     const order =  resp;
+
+  //     // Open Razorpay Checkout
+  //     const options = {
+  //       key: 'rzp_live_tG7YenWAYDDai9', // Replace with your Razorpay key_id
+  //       amount: resp.data.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+  //       currency: resp.data.currency,
+  //       name: 'Acme Corp',
+  //       description: 'Test Transaction',
+  //       order_id: 'order_IluGWxBm9U8zJ8', // This is the order_id created in the backend
+  //       callback_url: 'http://localhost:3000/payment-success', // Your success URL
+  //       prefill: {
+  //         name: 'Gaurav Kumar',
+  //         email: 'gaurav.kumar@example.com',
+  //         contact: '9999999999'
+  //       },
+  //       theme: {
+  //         color: '#F37254'
+  //       },
+  //     };
+
+  //     const rzp = new Razorpay(options);
+  //     rzp.open();
+  //   }).catch(err => {
+  //     console.log(err);
+  //   })
+  // }
 
   return (
     <div className="flex justify-center form-bg">
@@ -115,7 +198,17 @@ const RegistrationForm = props => {
           The total amount you need to pay is ₹199.
         </div>
         <button className="tutor-btn w-fit" onClick={handleSubmit(onSubmit)} disabled={pristine || submitting}>Pay Now</button>
+        <button className="tutor-btn w-fit" onClick={getApiResponse}>Mock Pay {displayRazorpay}</button>
       </div>
+      {displayRazorpay &&
+        <RenderRazorpay
+          amount={orderDetails.amount}
+          currency={orderDetails.currency}
+          orderId={orderDetails.orderId}
+          keyId={razorpayKey.id}
+          keySecret={razorpayKey.secret}
+        />
+      }
     </div>
   )
 }
