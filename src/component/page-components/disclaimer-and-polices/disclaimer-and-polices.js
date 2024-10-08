aae1a146908e317f55f076a7d0fe6ca6c9b549ca
Kishore Kuppan <kkuppan@vmware.com>
2024-10-08 19:40:09 +0530

    Merge remote-tracking branch 'origin/kishore-ui'

---
+++ src/component/page-components/disclaimer-and-polices/disclaimer-and-polices.js
@@ -0,0 +1,60 @@
+import { useNavigate } from "react-router-dom";
+
+function DisclaimerAndPolicies() {
+
+  const navigate = useNavigate();
+
+  function redirectToTandC() {
+    navigate(`/terms-and-conditions`);
+  }
+
+  function redirectToPrivacyPolicy() {
+    navigate(`/privacy-policy`);
+  }
+
+  return (
+    <div className="text-[#fff] pt-[32px] pb-[180px]">
+      <div>
+        <div className="text-xl text-[#ed970b]">Disclaimer</div>
+        <div className="flex flex-col gap-2">
+          <div className="pt-2">Coding Camp:</div>
+          <div>
+            The information provided by Coding Camp (“we,” “us,” or “our”) on our website is for general informational purposes only.
+            We strive to ensure that all information is accurate and up-to-date; however, we make no representations or warranties of any kind,
+            express or implied, about the completeness, accuracy, reliability, suitability, or availability of the information, products, services,
+            or related graphics contained on the website for any purpose. Any reliance you place on such information is strictly at your own risk.
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="pt-2">Educational Outcomes:</div>
+          <div>
+            Coding Camp is dedicated to providing quality education and training in website development. However, individual results may vary based on a
+            student’s effort, commitment, and the time they dedicate to their studies. We do not guarantee specific outcomes or job placements
+            upon completion of our courses. Success in the field of website development requires continuous learning and practice beyond the
+            completion of any single course or program.
+          </div>
+        </div>
+        <div className="py-3">
+          If you have any questions about this Privacy Policy, please contact us at: kishore.codingcamp@gmail.com
+        </div>
+      </div>
+      <div className="w-full text-xl pt-8 flex flex-wrap justify-center gap-7">
+        <div className="cursor-pointer" onClick={redirectToTandC}>Terms & Condition</div>
+        <div className="cursor-pointer" onClick={redirectToPrivacyPolicy}>Privacy Policy</div>
+      </div>
+      <div className="w-fit pt-8">
+        <div>Contact Us</div>
+        <div className="w-full text-xl flex flex-wrap items-center gap-2">
+          <div className="text-lg font-medium text-[#fff]">Email: </div>
+          <div className="text-sm text-[#fff]">kishore.codingcamp@gmail.com</div>
+        </div>
+        <div className="w-full text-xl flex flex-wrap items-center gap-2">
+          <div className="text-lg font-medium text-[#fff]">Address: </div>
+          <div className="text-sm text-[#fff]">No 41, 4th street devar nagar, vyasarpadi, chennai - 600039.</div>
+        </div>
+      </div>
+    </div>
+  )  
+}
+
+export default DisclaimerAndPolicies;
---
+++ src/component/page/privacy-policy/privacy-policy.js
@@ -0,0 +1,119 @@
+function PrivacyPolicyComponent() {
+  return (
+    <div className="p-6">
+      <div className="text-2xl font-semibold">Privacy Policy</div>
+      <div>
+        <div className="text-xl font-semibold py-3">Coding Camp</div>
+        <div>Coding Camp (“we,” “us,” or “our”) is committed to protecting your privacy. This Privacy Policy describes how we collect,
+          use, and disclose information when you visit our website or use our services. By accessing our website or using our services,
+          you agree to the terms of this Privacy Policy.
+        </div>
+      </div>
+      <div>
+        <div className="text-xl font-semibold py-3">Information We Collect</div>
+        <div>We collect the following types of information:</div>
+        <ol>
+          <li>
+            <span className="text-xl font-semibold">Personal Information:</span>
+            Information you provide to us directly, such as your name, email address, phone number, and payment
+            information when you register for our courses or contact us for support.
+          </li>
+          <li>
+            <span className="text-xl font-semibold">Usage Information:</span>
+            Information about how you use our website, such as your IP address, browser type, and pages visited.
+            We use cookies and similar tracking technologies to collect this information.
+          </li>
+          <li>
+            <span className="text-xl font-semibold">Communication Information:</span>
+            Information you provide when you communicate with us, including your feedback, inquiries, and other messages.
+          </li>
+        </ol>
+      </div>
+      <div>
+        <div className="text-xl font-semibold pt-3">How We Use Your Information</div>
+        <div className="text-xl font-semibold pb-3">We use the information we collect to:</div>
+        <ol>
+          <li>
+            Provide and improve our services, including customer support and course offerings.
+          </li>
+          <li>
+            Process payments and manage your account.
+          </li>
+          <li>
+            Communicate with you about our services, updates, and promotional offers.
+          </li>
+
+          <li>
+            Analyze usage patterns to enhance user experience and improve our website.
+          </li>
+          <li>
+            Comply with legal obligations and protect our rights and interests.
+          </li>
+        </ol>
+      </div>
+      <div>
+        <div className="text-xl font-semibold py-3">Refund Policy</div>
+        <div>
+          At Coding Camp, we strive to deliver quality education and ensure student satisfaction. If a class is canceled and not rescheduled,
+          you are eligible for a full refund. Please contact our member at kishore.codingcamp@gmail.com to request a refund.
+        </div>
+      </div>
+      <div>
+        <div className="text-xl font-semibold py-3">Educational Outcomes</div>
+        <div>
+        Our courses are designed to provide students with the skills needed to succeed in website development. However, individual outcomes
+        are based on students' efforts, and we do not guarantee specific results. Continuous learning and practice beyond our
+        courses are essential for success.
+        </div>
+      </div>
+      <div>
+        <div className="text-xl font-semibold py-3">Disclosure of Information</div>
+        <div>
+          <div>
+            We do not sell or rent your personal information to third parties. We may share your information with trusted partners
+            and service providers who assist us in operating our website and providing our services, subject to confidentiality agreements.
+          </div>
+          <div>
+            We may also disclose your information if required by law or in response to valid legal requests, such as subpoenas or court orders.
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="text-xl font-semibold py-3">Data Security</div>
+        <div>
+          We take reasonable measures to protect your information from unauthorized access, disclosure, alteration, or destruction. However,
+          no method of transmission over the Internet or electronic storage is completely secure, and we cannot guarantee absolute security.
+        </div>
+      </div>
+      <div>
+        <div className="text-xl font-semibold py-3">Your Rights</div>
+        <div>
+          You have the right to access, update, and delete your personal information. You can also object to the processing of
+          your personal information or request data portability. To exercise these rights, please contact us at kishore.codingcamp@gmail.com.
+        </div>
+      </div>
+      <div>
+        <div className="text-xl font-semibold py-3">Third-Party Links</div>
+        <div>
+          Our website may contain links to third-party websites. We are not responsible for the privacy practices of these
+          websites and encourage you to review their privacy policies before providing any personal information.
+        </div>
+      </div>
+      <div>
+        <div className="text-xl font-semibold py-3">Changes to This Privacy Policy</div>
+        <div>
+          We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on
+          this page. Your continued use of our website or services after any changes constitute your acceptance of the revised Privacy Policy.
+        </div>
+      </div>
+      <div>
+        <div className="text-xl font-semibold py-3">Contact Us</div>
+        <div>
+          If you have any questions or concerns about this Privacy Policy, please contact us at kishore.codingcamp@gmail.com.
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PrivacyPolicyComponent;
